6af3f8f66bbdea54fdbd3c10f1191a76bd96a516
Diwas10 <rimaldiwas10@gmail.com>
2023-01-12 21:33:59 +0545

    Include multiple colorschemes

---
+++ colorschemes/gruvbox.h
@@ -0,0 +1,14 @@
+/* Gruvbox colorscheme for dwm */
+
+static const char black[]          = "#282828";
+static const char darkblue[]       = "#282A36";
+static const char white[]          = "#EBDBB2";
+static const char mantle[]         = "#181825";
+static const char orange[]         = "#bd7149";
+static const char yellow[]         = "#D79921";
+
+static const char *colors[][3]     = {
+	                      /* fg        bg         border */
+	[SchemeNorm]       = { white,    black,    black    },
+	[SchemeSel]        = { black, orange, orange },
+};
